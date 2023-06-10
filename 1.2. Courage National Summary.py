d0d7b0645886f536c8efae53099a1a097957362f
james-helium <kh672@cantab.ac.uk>
2023-06-10 16:02:32 +0100

    get unweighted results to compare to old numbers

--- 1.2. Courage National Summary.py
+++ 1.2. Courage National Summary.py
@@ -7,89 +7,146 @@
 import pandas as pd
 
 # Read the data
-filtered_full_sample = pd.read_csv('data/filtered_full_sample.csv').drop('Unnamed: 0', axis=1)
-un_populations = pd.read_csv('data/un_populations.csv')
+filtered_full_sample = pd.read_csv("data/filtered_full_sample.csv").drop(
+    "Unnamed: 0", axis=1
+)
+un_populations = pd.read_csv("data/un_populations.csv")
 # process UN reference data
 
-un_populations = un_populations[['Location','Time','AgeGrp','PopMale','PopFemale','PopTotal']]
+un_populations = un_populations[
+    ["Location", "Time", "AgeGrp", "PopMale", "PopFemale", "PopTotal"]
+]
 # only keep 2017-2021, when our study collected data
 un_populations = un_populations[un_populations.Time.between(2017, 2021)]
 # take the mean of 2017-2021
-un_populations = un_populations.groupby(['Location','AgeGrp']).mean().reset_index()
+un_populations = un_populations.groupby(["Location", "AgeGrp"]).mean().reset_index()
 
 # regourp the population into age groups 0-14, 15-19, 20-24, 25-49, 50-69, 70-99
-pop_00_14 = un_populations[un_populations.AgeGrp.isin(['0-4','5-9','10-14'])]
-pop_15_19 = un_populations[un_populations.AgeGrp.isin(['15-19'])]
-pop_20_24 = un_populations[un_populations.AgeGrp.isin(['20-24'])]
-pop_25_49 = un_populations[un_populations.AgeGrp.isin(['25-29','30-34','35-39','40-44','45-49'])]
-pop_50_69 = un_populations[un_populations.AgeGrp.isin(['50-54','55-59','60-64','65-69'])]
-pop_70_99 = un_populations[un_populations.AgeGrp.isin(['70-74','75-79','80-84','85-89','90-94','95-99'])]
+pop_00_14 = un_populations[un_populations.AgeGrp.isin(["0-4", "5-9", "10-14"])]
+pop_15_19 = un_populations[un_populations.AgeGrp.isin(["15-19"])]
+pop_20_24 = un_populations[un_populations.AgeGrp.isin(["20-24"])]
+pop_25_49 = un_populations[
+    un_populations.AgeGrp.isin(["25-29", "30-34", "35-39", "40-44", "45-49"])
+]
+pop_50_69 = un_populations[
+    un_populations.AgeGrp.isin(["50-54", "55-59", "60-64", "65-69"])
+]
+pop_70_99 = un_populations[
+    un_populations.AgeGrp.isin(["70-74", "75-79", "80-84", "85-89", "90-94", "95-99"])
+]
 
 # sum up the population of 0-14, 15-19, 20-24, 25-49, 50-69, 70-99
-pop_00_14 = pop_00_14.groupby('Location').sum().reset_index()
-pop_00_14['AgeGrp'] = '0-14'
-pop_00_14 = pop_00_14[['Location','AgeGrp','PopMale','PopFemale','PopTotal']]
-pop_15_19 = pop_15_19[['Location','AgeGrp','PopMale','PopFemale','PopTotal']]
-pop_20_24 = pop_20_24[['Location','AgeGrp','PopMale','PopFemale','PopTotal']]
-pop_25_49 = pop_25_49.groupby('Location').sum().reset_index()
-pop_25_49['AgeGrp'] = '25-49'
-pop_25_49 = pop_25_49[['Location','AgeGrp','PopMale','PopFemale','PopTotal']]
-pop_50_69 = pop_50_69.groupby('Location').sum().reset_index()
-pop_50_69['AgeGrp'] = '50-69'
-pop_50_69 = pop_50_69[['Location','AgeGrp','PopMale','PopFemale','PopTotal']]
-pop_70_99 = pop_70_99.groupby('Location').sum().reset_index()
-pop_70_99['AgeGrp'] = '70-99'
-pop_70_99 = pop_70_99[['Location','AgeGrp','PopMale','PopFemale','PopTotal']]
+pop_00_14 = pop_00_14.groupby("Location").sum().reset_index()
+pop_00_14["AgeGrp"] = "0-14"
+pop_00_14 = pop_00_14[["Location", "AgeGrp", "PopMale", "PopFemale", "PopTotal"]]
+pop_15_19 = pop_15_19[["Location", "AgeGrp", "PopMale", "PopFemale", "PopTotal"]]
+pop_20_24 = pop_20_24[["Location", "AgeGrp", "PopMale", "PopFemale", "PopTotal"]]
+pop_25_49 = pop_25_49.groupby("Location").sum().reset_index()
+pop_25_49["AgeGrp"] = "25-49"
+pop_25_49 = pop_25_49[["Location", "AgeGrp", "PopMale", "PopFemale", "PopTotal"]]
+pop_50_69 = pop_50_69.groupby("Location").sum().reset_index()
+pop_50_69["AgeGrp"] = "50-69"
+pop_50_69 = pop_50_69[["Location", "AgeGrp", "PopMale", "PopFemale", "PopTotal"]]
+pop_70_99 = pop_70_99.groupby("Location").sum().reset_index()
+pop_70_99["AgeGrp"] = "70-99"
+pop_70_99 = pop_70_99[["Location", "AgeGrp", "PopMale", "PopFemale", "PopTotal"]]
 # put them back together
-un_populations = pd.concat([pop_00_14,pop_15_19,pop_20_24,pop_25_49,pop_50_69,pop_70_99])
+un_populations = pd.concat(
+    [pop_00_14, pop_15_19, pop_20_24, pop_25_49, pop_50_69, pop_70_99]
+)
 
 # get total population
-pop_total = un_populations.groupby('Location').sum().reset_index()[['Location', 'PopTotal']]
+pop_total = (
+    un_populations.groupby("Location").sum().reset_index()[["Location", "PopTotal"]]
+)
 # ratio of population in each age-sex group
-un_populations = un_populations.merge(pop_total, on='Location', how='left')
-un_populations.rename(columns={'PopTotal_x':'AgeTotal', 'PopTotal_y':'PopTotal'}, inplace=True)
-un_populations['PopMale'] = un_populations['PopMale']/un_populations['PopTotal']
-un_populations['PopFemale'] = un_populations['PopFemale']/un_populations['PopTotal']
-un_populations = un_populations[['Location','AgeGrp','PopMale','PopFemale']]
+un_populations = un_populations.merge(pop_total, on="Location", how="left")
+un_populations.rename(
+    columns={"PopTotal_x": "AgeTotal", "PopTotal_y": "PopTotal"}, inplace=True
+)
+un_populations["PopMale"] = un_populations["PopMale"] / un_populations["PopTotal"]
+un_populations["PopFemale"] = un_populations["PopFemale"] / un_populations["PopTotal"]
+un_populations = un_populations[["Location", "AgeGrp", "PopMale", "PopFemale"]]
 # standardise country names
-un_populations.loc[un_populations['Location']=='China, Hong Kong SAR', 'Location'] = 'Hong Kong'
-un_populations.loc[un_populations['Location']=='China, Taiwan Province of China', 'Location'] = 'Taiwan'
-un_populations.loc[un_populations['Location']=='Iran (Islamic Republic of)', 'Location'] = 'Iran'
-un_populations.loc[un_populations['Location']=='Republic of Korea', 'Location'] = 'South Korea'
-un_populations.loc[un_populations['Location']=='Russian Federation', 'Location'] = 'Russia'
-un_populations.loc[un_populations['Location']=='Türkiye', 'Location'] = 'Turkey'
-un_populations.loc[un_populations['Location']=='United Kingdom of Great Britain and Northern Ireland', 'Location'] = 'United Kingdom'
-un_populations.loc[un_populations['Location']=='United States of America', 'Location'] = 'United States'
-un_populations.loc[un_populations['Location']=='Venezuela (Bolivarian Republic of)', 'Location'] = 'Venezuela'
-un_populations.loc[un_populations['Location']=='Viet Nam', 'Location'] = 'Vietnam'
-un_populations.set_index('Location', inplace=True)
+un_populations.loc[
+    un_populations["Location"] == "China, Hong Kong SAR", "Location"
+] = "Hong Kong"
+un_populations.loc[
+    un_populations["Location"] == "China, Taiwan Province of China", "Location"
+] = "Taiwan"
+un_populations.loc[
+    un_populations["Location"] == "Iran (Islamic Republic of)", "Location"
+] = "Iran"
+un_populations.loc[
+    un_populations["Location"] == "Republic of Korea", "Location"
+] = "South Korea"
+un_populations.loc[
+    un_populations["Location"] == "Russian Federation", "Location"
+] = "Russia"
+un_populations.loc[un_populations["Location"] == "Türkiye", "Location"] = "Turkey"
+un_populations.loc[
+    un_populations["Location"]
+    == "United Kingdom of Great Britain and Northern Ireland",
+    "Location",
+] = "United Kingdom"
+un_populations.loc[
+    un_populations["Location"] == "United States of America", "Location"
+] = "United States"
+un_populations.loc[
+    un_populations["Location"] == "Venezuela (Bolivarian Republic of)", "Location"
+] = "Venezuela"
+un_populations.loc[un_populations["Location"] == "Viet Nam", "Location"] = "Vietnam"
+un_populations.set_index("Location", inplace=True)
 # only keep countries in our sample
-un_populations = un_populations.loc[filtered_full_sample['geo_country'].unique()]
+un_populations = un_populations.loc[filtered_full_sample["geo_country"].unique()]
 # get average courage score of each country, age-sex groups
 
 # first, we recode the demo_age to age groups
-filtered_full_sample['demo_age'] = pd.to_numeric(filtered_full_sample['demo_age'], errors='coerce')
-filtered_full_sample['demo_age'] = pd.cut(filtered_full_sample['demo_age'], bins=[0,14,19,24,49,69,99], labels=['0-14','15-19','20-24','25-49','50-69','70-99'])
+filtered_full_sample["demo_age"] = pd.to_numeric(
+    filtered_full_sample["demo_age"], errors="coerce"
+)
+filtered_full_sample["demo_age"] = pd.cut(
+    filtered_full_sample["demo_age"],
+    bins=[0, 14, 19, 24, 49, 69, 99],
+    labels=["0-14", "15-19", "20-24", "25-49", "50-69", "70-99"],
+)
 
 # summarise magic_COUR as mean, group by geo_country, demo_gender, demo_age
-courage_summary = filtered_full_sample.groupby(['geo_country', 'demo_gender', 'demo_age']).mean().reset_index()
-courage_summary = courage_summary[courage_summary.demo_gender=='female'].merge(
-    courage_summary[courage_summary.demo_gender=='male']
-    , on=['geo_country', 'demo_age'])
-courage_summary = courage_summary[['geo_country', 'demo_age', 'magic_COUR_x', 'magic_COUR_y']]
-courage_summary.columns = ['Location', 'AgeGrp', 'CourFemale', 'CourMale']
+courage_summary = (
+    filtered_full_sample.groupby(["geo_country", "demo_gender", "demo_age"])
+    .mean()
+    .reset_index()
+)
+courage_summary = courage_summary[courage_summary.demo_gender == "female"].merge(
+    courage_summary[courage_summary.demo_gender == "male"],
+    on=["geo_country", "demo_age"],
+)
+courage_summary = courage_summary[
+    ["geo_country", "demo_age", "magic_COUR_x", "magic_COUR_y"]
+]
+courage_summary.columns = ["Location", "AgeGrp", "CourFemale", "CourMale"]
 
 # merge with population data
-courage_summary = courage_summary.merge(un_populations, on=['Location', 'AgeGrp'])
+courage_summary = courage_summary.merge(un_populations, on=["Location", "AgeGrp"])
 
 
 # calculate weighted average courage score
 
 courage_summary.CourFemale = courage_summary.CourFemale * courage_summary.PopFemale
 courage_summary.CourMale = courage_summary.CourMale * courage_summary.PopMale
-courage_summary = courage_summary.groupby('Location').sum().reset_index()
-courage_summary['weighted_courage'] = courage_summary.CourFemale + courage_summary.CourMale
+courage_summary = courage_summary.groupby("Location").sum().reset_index()
+courage_summary["weighted_courage"] = (
+    courage_summary.CourFemale + courage_summary.CourMale
+)
+courage_summary["unweighted_courage"] = (
+    filtered_full_sample[["geo_country", "magic_COUR"]]
+    .groupby("geo_country")
+    .mean()
+    .reset_index()["magic_COUR"]
+)
 # save to csv
-courage_summary = courage_summary[['Location', 'weighted_courage']]
-courage_summary.columns = ['Country', 'Weighted Average']
-courage_summary.to_csv('data/national_courage.csv', index=False)
+courage_summary = courage_summary[
+    ["Location", "weighted_courage", "unweighted_courage"]
+]
+courage_summary.columns = ["Country", "Weighted Average", "Unweighted Average"]
+courage_summary.to_csv("data/national_courage.csv", index=False)
--- 1.7. Combine Data Sources.py
+++ 1.7. Combine Data Sources.py
@@ -1,5 +1,5 @@
 # this script combines the data sources into one data frame
-##### IMPORTANT: manually add hofstede, terrorism, innovation data before the next step #####
+##### IMPORTANT: hofstede, terrorism, innovation data are manually copied from books and websites #####
 
 # initialise
 
@@ -11,14 +11,19 @@ main_controls = pd.read_csv("data/main_controls_extract.csv")
 ipums_controls = pd.read_csv("data/ipums_controls_extract.csv")
 ipums_idv = pd.read_csv("data/ipums_idv_extract.csv")
 wvs_idv = pd.read_csv("data/wvs_idv_extract.csv")
+manual_data = pd.read_csv("data/manual_data.csv")
 # get country names and code from main controls
 country_names = main_controls[["Country Name", "Country Code"]].drop_duplicates()
 # standadise national courange country names and code
-national_courage = national_courage.merge(country_names, left_on="Country", right_on="Country Name", how="left")
+national_courage = national_courage.merge(
+    country_names, left_on="Country", right_on="Country Name", how="left"
+)
 national_courage.loc[national_courage["Country"] == "Egypt", "Country Code"] = "EGY"
 national_courage.loc[national_courage["Country"] == "Hong Kong", "Country Code"] = "HKG"
 national_courage.loc[national_courage["Country"] == "Iran", "Country Code"] = "IRN"
-national_courage.loc[national_courage["Country"] == "South Korea", "Country Code"] = "KOR"
+national_courage.loc[
+    national_courage["Country"] == "South Korea", "Country Code"
+] = "KOR"
 national_courage.loc[national_courage["Country"] == "Taiwan", "Country Code"] = "TWN"
 national_courage.loc[national_courage["Country"] == "Russia", "Country Code"] = "RUS"
 national_courage.loc[national_courage["Country"] == "Slovakia", "Country Code"] = "SVK"
@@ -32,9 +37,11 @@ national_courage = national_courage.rename(columns={"Country": "Country Name"})
 # set Country and Country Code as index
 national_courage = national_courage.set_index(["Country Name", "Country Code"])
 # rename columns
-national_courage.columns = ['national_courage']
+national_courage.columns = ["national_courage", "national_courage_unweighted"]
 # standardise ipums controls with country names and code
-ipums_controls = ipums_controls.merge(country_names, left_on="Country", right_on="Country Name", how="left")
+ipums_controls = ipums_controls.merge(
+    country_names, left_on="Country", right_on="Country Name", how="left"
+)
 # mannually set Country Code for missing values: 'Egypt', 'Iran', 'Russia', 'Turkey', 'Venezuela'
 ipums_controls.loc[ipums_controls["Country"] == "Egypt", "Country Code"] = "EGY"
 ipums_controls.loc[ipums_controls["Country"] == "Iran", "Country Code"] = "IRN"
@@ -50,17 +57,19 @@ ipums_controls = ipums_controls.rename(columns={"Country": "Country Name"})
 ipums_controls = ipums_controls.set_index(["Country Name", "Country Code"])
 # rename columns
 ipums_controls.columns = [
-    'index',
-    'ipums_edu_tertiary_attain',
-    'ipums_edu_adult_literacy',
-    'ipums_urbanisation'
+    "index",
+    "ipums_edu_tertiary_attain",
+    "ipums_edu_adult_literacy",
+    "ipums_urbanisation",
 ]
 # drop index
 ipums_controls = ipums_controls.drop("index", axis=1)
 # standardise ipums idv with country names and code in ipums_controls
 ipums_idv = ipums_idv.merge(
     ipums_controls.index.to_frame().reset_index(drop=True),
-    left_on="Country", right_on='Country Name', how="left"
+    left_on="Country",
+    right_on="Country Name",
+    how="left",
 )
 # manually add slovakia and slovenia
 ipums_idv.loc[ipums_idv["Country"] == "Slovakia", "Country Code"] = "SVK"
@@ -73,11 +82,13 @@ ipums_idv = ipums_idv.rename(columns={"Country": "Country Name"})
 # set Country and Country Code as index
 ipums_idv = ipums_idv.set_index(["Country Name", "Country Code"])
 # rename columns
-ipums_idv.columns = ['index', 'ipums_idv']
+ipums_idv.columns = ["index", "ipums_idv"]
 # drop index
 ipums_idv = ipums_idv.drop("index", axis=1)
 # standardise wvs idv with country names and code
-wvs_idv = wvs_idv.merge(country_names, left_on="Country", right_on="Country Code", how="left")
+wvs_idv = wvs_idv.merge(
+    country_names, left_on="Country", right_on="Country Code", how="left"
+)
 # manually set the Country Name of TWN and NIR
 wvs_idv.loc[wvs_idv["Country"] == "TWN", "Country Name"] = "Taiwan"
 wvs_idv.loc[wvs_idv["Country"] == "NIR", "Country Name"] = "Northern Ireland"
@@ -89,12 +100,12 @@ wvs_idv = wvs_idv.rename(columns={"Country": "Country Code"})
 # set Country and Country Code as index
 wvs_idv = wvs_idv.set_index(["Country Name", "Country Code"])
 # rename columns
-wvs_idv.columns = ['index', 'wvs_idv']
+wvs_idv.columns = ["index", "wvs_idv"]
 # drop index
 wvs_idv = wvs_idv.drop("index", axis=1)
 # merge all data frames
 
-# set the index of national courage as the standard country name 
+# set the index of national courage as the standard country name
 country_names = national_courage.index.to_frame().reset_index(drop=True)
 
 # set country code as the index of all data frames
@@ -112,17 +123,24 @@ national_courage = national_courage.drop("Country Name", axis=1)
 
 # merge all data frames
 all_national_data = pd.concat(
-    [main_controls, ipums_controls, ipums_idv, wvs_idv, national_courage],
-    axis=1
+    [main_controls, ipums_controls, ipums_idv, wvs_idv, national_courage], axis=1
 )
 # add country names back to the data frame
-all_national_data = all_national_data.merge(country_names, left_index=True, right_on="Country Code", how="left")
+all_national_data = all_national_data.merge(
+    country_names, left_index=True, right_on="Country Code", how="left"
+)
 # drop column index
 all_national_data = all_national_data.drop("index", axis=1)
 
 # only keep the countries that are in national_courage
-all_national_data = all_national_data[all_national_data["Country Code"].isin(national_courage.index.get_level_values(0))]
+all_national_data = all_national_data[
+    all_national_data["Country Code"].isin(national_courage.index.get_level_values(0))
+]
+
 # set country name and country code as index
+all_national_data = all_national_data.merge(
+    manual_data, on=["Country Name", "Country Code"], how="left"
+)
 all_national_data = all_national_data.set_index(["Country Name", "Country Code"])
 
-all_national_data.to_csv('data/all_national_data.csv')
+all_national_data.to_csv("data/all_national_data.csv")
