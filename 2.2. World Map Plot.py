24fd8cd22905e9911177d2ace79871675683d9fe
james-helium <kh672@cantab.ac.uk>
2023-06-29 09:48:35 +0100

    improve world-map plot

--- 2.2. World Map Plot.py
+++ 2.2. World Map Plot.py
@@ -19,19 +19,14 @@ world_courage = world.merge(
     how='left', 
     on='Country Code'
 )
-# turn the national_courage score to a scale from 0 to 10 using softmax
-world_courage['national_courage'] = np.exp(world_courage['national_courage']) / np.sum(np.exp(world_courage['national_courage'])) * 10
+
+# transform the scale for easier visualization
+world_courage['national_courage'] = world_courage['national_courage'] ** 5
 # Create a plot of the world map with courage data colored differently
 fig, ax = plt.subplots(figsize=(96,48))
-world_courage.plot(column='national_courage', cmap='Reds', legend=True, ax=ax)
+world_courage.plot(column='national_courage', cmap='Reds', legend=False, ax=ax)
 # remove the axis 
 ax.axis('off')
-# make the colourbar smaller
-cbar = ax.get_figure().get_axes()[1]
-cbar.set_ylabel('National Courage (0-10)', fontsize=20)
-cbar.set_yticklabels([i for i in range(10)], fontsize=20)
-# set cbar size
-cbar.set_position([0.16, 0.3, 0.1, 0.2])
 
 # save the plot
 plt.savefig('results/world_map.png', dpi=100)
